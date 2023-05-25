0f44d722baa54c1f96e9005d439c902447cb73cc
laman-isg <laman.isgandar@gmail.com>
2023-05-25 12:43:36 +0400

    change.commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 new WOW().init();
+
+$(function(){
+  
+    const $body = $('body');
+    
+    $('.js-hambergur-btn').on('click',function(){
+      
+      if($body.hasClass('is-active')){
+        $body.addClass('is-close');
+      }
+      
+      // transitionの動きを検知して、transitionが終了後、関数の処理を行う。
+      $body.on('transitionend',function(){
+        $body.removeClass('is-close'); 
+      });
+      
+      $body.toggleClass('is-active');
+      
+      return false;
+    });
+    
+  });
