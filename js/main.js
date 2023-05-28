bc1dde1c9be8685518716f34d1606cb8a643e651
laman-isg <laman.isgandar@gmail.com>
2023-05-29 00:19:25 +0400

    change.commit

--- js/main.js
+++ js/main.js
@@ -1,51 +1,52 @@
 new WOW().init();
 
 
-$(window).ready(function () {
-  $("#menu-btn").on("click", function () {
-    $("body").toggleClass("menu-active");
+  $(window).ready(function() {
+    $("#menu-btn").on("click", function() {
+      $("body").toggleClass("menu-active");
+    });
   });
-});
-window.onload = function () {
-  var scrollElem = document.getElementById("scrollToTop");
-  window.onscroll = function () {
-    if (this.scrollY > 400) {
-      scrollElem.style.display = "block";
-    } else {
-      scrollElem.style.display = "none";
+  window.onload = function() {
+    var scrollElem = document.getElementById("scrollToTop");
+    window.onscroll = function() {
+        if (this.scrollY > 400) {
+            scrollElem.style.display = "block";
+        } else {
+            scrollElem.style.display = "none";
+        }
+    }
+    scrollElem.onclick = function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     }
   }
-  scrollElem.onclick = function () {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  
+  var hamb = document.querySelector('.hamburger'); 
+  var menu = document.querySelector('.menu');
+  var list = document.getElementById('menu');
+  
+  
+  function closeMenu() {
+      menu.style.width = '0px';
   }
-}
-
-var hamb = document.querySelector('.hamburger');
-var menu = document.querySelector('.menu');
-var list = document.getElementById('menu');
-
-
-function closeMenu() {
-  menu.style.width = '0px';
-}
-
-function openMenu() {
-  menu.style.width = '100%';
-}
-
-hamb.addEventListener('click', function () {
+  
+  function openMenu() {
+      menu.style.width = '100%';
+  }
+  
+  hamb.addEventListener('click', function() {
   var active = hamb.classList.toggle('hamburger--active');
-
-  if (active) {
-    hamb.addEventListener('click', openMenu());
-  } else {
-    hamb.addEventListener('click', closeMenu());
-  }
-});
-
-list.addEventListener('click', function () {
+      
+  if(active) {
+               hamb.addEventListener('click', openMenu());
+      } else {
+               hamb.addEventListener('click', closeMenu());
+      }
+  });
+  
+  list.addEventListener('click', function() { 
   closeMenu();
   var active = hamb.classList.toggle('hamburger--active');
-});
-
-
+  });
+  
+  
+  
