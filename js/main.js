c2642f38887f2a22bd846928f36d7e3dd7282abc
laman-isg <laman.isgandar@gmail.com>
2023-05-28 23:59:34 +0400

    change.commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,51 @@
 new WOW().init();
 
-$(function(){
-  
-    const $body = $('body');
-    
-    $('.js-hambergur-btn').on('click',function(){
-      
-      if($body.hasClass('is-active')){
-        $body.addClass('is-close');
-      }
-      
-      // transitionの動きを検知して、transitionが終了後、関数の処理を行う。
-      $body.on('transitionend',function(){
-        $body.removeClass('is-close'); 
-      });
-      
-      $body.toggleClass('is-active');
-      
-      return false;
-    });
-    
+
+$(window).ready(function () {
+  $("#menu-btn").on("click", function () {
+    $("body").toggleClass("menu-active");
   });
+});
+window.onload = function () {
+  var scrollElem = document.getElementById("scrollToTop");
+  window.onscroll = function () {
+    if (this.scrollY > 400) {
+      scrollElem.style.display = "block";
+    } else {
+      scrollElem.style.display = "none";
+    }
+  }
+  scrollElem.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}
+
+var hamb = document.querySelector('.hamburger');
+var menu = document.querySelector('.menu');
+var list = document.getElementById('menu');
+
+
+function closeMenu() {
+  menu.style.width = '0px';
+}
+
+function openMenu() {
+  menu.style.width = '100%';
+}
+
+hamb.addEventListener('click', function () {
+  var active = hamb.classList.toggle('hamburger--active');
+
+  if (active) {
+    hamb.addEventListener('click', openMenu());
+  } else {
+    hamb.addEventListener('click', closeMenu());
+  }
+});
+
+list.addEventListener('click', function () {
+  closeMenu();
+  var active = hamb.classList.toggle('hamburger--active');
+});
+
+
